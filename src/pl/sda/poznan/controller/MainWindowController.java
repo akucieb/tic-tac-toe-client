801133935de36305fd3916a271a945d3799a352f
Anita Kucieba <akucieba@gmail.com>
2018-04-15 10:53:31 +0200

    initial commit

---
+++ src/pl/sda/poznan/controller/MainWindowController.java
@@ -0,0 +1,54 @@
+package pl.sda.poznan.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import pl.sda.poznan.viewmodel.ConnectionDialogViewModel;
+
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class MainWindowController {
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
+    public void handleClick(MouseEvent mouseEvent) {
+        Label source = (Label) mouseEvent.getSource();
+        System.out.println(source.getId());
+    }
+
+    public void connectToServer(ActionEvent actionEvent) {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("ConnectionDialogWindow.fxml"));
+
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle("Polacz do serwera");
+        dialog.setHeaderText("Uzupelnij dane");
+
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> optionalType = dialog.showAndWait();
+        optionalType.ifPresent(buttonType -> {
+            ConnectionDialogController controller = fxmlLoader.getController();
+            ConnectionDialogViewModel connectionDetails = controller.getConnectionDetails();
+            connectToServer(connectionDetails);
+        });
+    }
+
+    public void connectToServer(ConnectionDialogViewModel viewModel) {
+        logger.log(Level.INFO, String.format("Trying to connect to server at address %s with username %s", viewModel.getServerAddress(), viewModel.getPlayerName()));
+    }
+}
