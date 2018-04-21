d7ef9a40f6c8506a8783ec6eb8591b8684c8581c
Anita Kucieba <akucieba@gmail.com>
2018-04-21 10:30:58 +0200

    Added connection to server

--- src/pl/sda/poznan/controller/MainWindowController.java
+++ src/pl/sda/poznan/controller/MainWindowController.java
@@ -1,34 +1,42 @@
 package pl.sda.poznan.controller;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import pl.sda.poznan.Message;
+import pl.sda.poznan.Transmission;
 import pl.sda.poznan.viewmodel.ConnectionDialogViewModel;
 
-
-import java.io.IOException;
-import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-
 public class MainWindowController {
+
     private final Logger logger = Logger.getLogger(getClass().getName());
+    private Transmission transmission;
 
     public void handleClick(MouseEvent mouseEvent) {
         Label source = (Label) mouseEvent.getSource();
         System.out.println(source.getId());
     }
 
-    public void connectToServer(ActionEvent actionEvent) {
+    /**
+     * Metoda wykonujaca sie przy klinieciu przycisku polacz do serwera z Menu Game
+     */
+    public void connectToServerAction(ActionEvent actionEvent) {
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("ConnectionDialogWindow.fxml"));
 
         Dialog<ButtonType> dialog = new Dialog<>();
-        dialog.setTitle("Polacz do serwera");
+        dialog.setTitle("Połącz do serwera");
         dialog.setHeaderText("Uzupelnij dane");
 
         try {
@@ -36,7 +44,6 @@ public class MainWindowController {
         } catch (IOException e) {
             return;
         }
-
         dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
         dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
 
@@ -49,6 +56,39 @@ public class MainWindowController {
     }
 
     public void connectToServer(ConnectionDialogViewModel viewModel) {
-        logger.log(Level.INFO, String.format("Trying to connect to server at address %s with username %s", viewModel.getServerAddress(), viewModel.getPlayerName()));
+        logger.log(Level.INFO, String.format(
+                "Trying to connect to server at address %s with username %s", viewModel.getServerAddress(),
+                viewModel.getPlayerName()));
+        String[] address = viewModel.getServerAddress().split(":");
+        String host = address[0];
+        int port = Integer.parseInt(address[1]);
+        // todo: add server address validation - display error dialog if sth is wrong
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress(host, port));
+            this.transmission = new Transmission(socket);
+
+            transmission.sendObject(Message.builder()
+                    .header("CONNECT")
+                    .data(viewModel.getPlayerName())
+                    .build());
+
+            try {
+                Object o = transmission.readObject();
+                Message message = (Message) o;
+                logger.info(String.format("Received message %s", message.getHeader()));
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            logger.log(Level.INFO, "Cannot connect to server: " + e.getMessage());
+            // todo: wyswietl okno z napisem "Nie udalo sie podlaczyc do serwera"
+            // Skorzystaj z obiektu Alert
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Błąd");
+            alert.setContentText("Nie udało się połączyć z serwerem");
+            alert.showAndWait();
+        }
     }
+
 }
